1fc992488865eb63a632f0ef0b4f8082308dbfb8
ArMinasyan <46679050+ArMinasyan@users.noreply.github.com>
2019-10-05 17:07:12 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var server = require('http');
 var app=require('express')();
-var i=require('./routes/insert_data.js');
 var insert=require('./routes/insert_data');
 var get=require('./routes/get_data');
 //var update=require('./routes/update_data');
@@ -8,6 +7,7 @@ var get=require('./routes/get_data');
 app.use('/insert',insert);
 app.use('/get',get);
 //app.use('/update',update);
+
 server.createServer(app,function (req, res) {}).listen(8080, function () {
     console.log('Start...');
 });
